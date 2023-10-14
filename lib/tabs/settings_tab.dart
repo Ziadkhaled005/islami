634141d168264cf482513bf8ffdce12245712eef
Ziad Khaled <135760232+Ziadkhaled005@users.noreply.github.com>
2023-10-14 12:52:43 +0300

    Update settings_tab.dart

--- lib/tabs/settings_tab.dart
+++ lib/tabs/settings_tab.dart
@@ -6,7 +6,7 @@ class SettingsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.pinkAccent,
+      color: Colors.blue,
     );
   }
 }
